eef4cb7fda9e2ccf2f23d02d05e69271258879ee
lolaakhmedova <139182124+lolaakhmedova@users.noreply.github.com>
2026-09-21 20:44:03 -0400

    Update HelloWorld.java to print additional messages

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -2,6 +2,8 @@ package day01;
 
 public class HelloWorld {
     static void main() {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello World World");
     }
 }
